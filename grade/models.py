94c39a5b75c083d9b51892580c15879ce6dc9c44
Vova Stoliarchuk <vovkastoliarchuk@gmail.com>
2024-09-03 15:03:53 +0000

    Grades work

--- grade/models.py
+++ grade/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from bases.models import Lesson
+from django.contrib.auth.models import User
 
 class Day_grade(models.Model):  
     score = models.IntegerField("Оцінка за урок")
     leson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='day_grades')
+    student_grade = models.ManyToManyField(User, related_name='day_grade', blank=True)
